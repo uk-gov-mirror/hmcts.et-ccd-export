9bb0dc675cc89adc668b8f3bdab3034b21cbc604
Gary Taylor <gary.taylor@hismessages.com>
2019-07-18 17:23:29 +0100

    RST-1934 CCD - Case creation of multiple Added ccd response to sentry message

--- lib/ccd_client_sentry_error_middleware.rb
+++ lib/ccd_client_sentry_error_middleware.rb
@@ -1,7 +1,8 @@
 require 'raven'
+require 'et_ccd_client'
 class CcdClientSentryErrorMiddleware
   def call(ex, context)
-    Raven.context.extra.merge! ccd_response: ex.response.body if ex.is_a?(EtCcdClient::Exception::Base)
+    Raven.context.extra.merge! ccd_response: ex.response.body if ex.is_a?(EtCcdClient::Exceptions::Base)
     yield
   end
 end
