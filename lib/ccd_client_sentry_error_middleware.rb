8f53703e2969294196bb5a1c6faec7fd16270606
Gary Taylor <gary.taylor@hismessages.com>
2019-08-01 13:44:13 +0100

    Error handling should now include the request that caused the error

--- lib/ccd_client_sentry_error_middleware.rb
+++ lib/ccd_client_sentry_error_middleware.rb
@@ -2,6 +2,15 @@ require 'raven'
 require 'et_ccd_client'
 class CcdClientSentryErrorMiddleware
   def call(ex, context)
-    Raven.context.extra.merge! ccd_response: ex.response.body if ex.is_a?(EtCcdClient::Exceptions::Base)
+    return unless ex.is_a?(EtCcdClient::Exceptions::Base)
+    req = {}
+    if ex&.request
+      req[:url] = ex.request.url
+      req[:method] = ex.request.method
+      req[:payload] = ex.request.payload
+      req[:headers] = ex.request.headers
+      req[:cookies] = ex.request.cookies
+    end
+    Raven.context.extra.merge! ccd_response: ex.response.body, ccd_request: req
   end
 end
