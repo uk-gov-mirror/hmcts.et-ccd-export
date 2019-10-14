dc19742688b389af0f4536606fca98d556effdcb
garytaylor <gary.taylor@hismessages.com>
2019-10-14 10:58:35 +0100

    RST-2265 (CCD) - JSON::ParserErrorSidekiq/ExportMultiplesHeaderWorker (#61)

--- app/workers/export_multiples_header_worker.rb
+++ app/workers/export_multiples_header_worker.rb
@@ -18,6 +18,13 @@ class ExportMultiplesHeaderWorker
     events_service.send_multiples_claim_exported_event(export_id: export_id, sidekiq_job_data: job_hash, case_id: created_case['id'], case_reference: created_case.dig('case_data', 'multipleReference'), case_type_id: case_type_id)
   end
 
+  sidekiq_retries_exhausted do |msg, ex|
+    _primary_reference, _respondent_name, _case_references, _case_type_id, export_id = args
+    job_data = msg.except('args', 'class').merge(ex.try(:job_hash) || {})
+    ApplicationEventsService.send_claim_failed_event(export_id: export_id, sidekiq_job_data: job_data)
+    raise ClaimNotExportedException
+  end
+
   private
 
   attr_accessor :events_service, :service
